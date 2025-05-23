21dbdd473a706bbf150c974ba2ef8fa34ac3d9ed
Pia Pacana <pacana.pia123@gmail.com>
2025-05-23 18:16:59 +0800

    I added check-out,order-confirm, and order-tracking

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Checkout from './pages/checkout/co.js'; 
+import OrderConfirm from './pages/orderconfirm/oc.js';
+import OrderTrack from './pages/ordertracking/ot.js';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/checkout" element={<Checkout />} />
+        <Route path="/orderconfirm" element={<OrderConfirm />} />
+        <Route path="/ordertrack" element={<OrderTrack />} />
+      </Routes>
+    </Router>
   );
 }
 
