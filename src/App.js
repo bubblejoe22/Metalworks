3e0734ca89f86ea0094323167cc17afada43edba
Raizhi Sarino <sarino.raizhi25@gmail.com>
2025-05-23 17:53:11 +0800

    I added privacy policy, terms and conditions and FAQ

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import FAQ from './pages/FAQ/Faq.js';
+import TermsAndConditions from './pages/TermsAndConditions/TermsAndConditions.js';
+import PrivacyPolicy from './pages/PrivacyPolicy/PrivacyPolicy.js';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+ 
+      
+      <Routes>
+        <Route path="/FAQ" element={<FAQ />} />
+        <Route path="/TermsAndConditions" element={<TermsAndConditions />} />
+        <Route path="/PrivacyPolicy" element={<PrivacyPolicy />} />
+
+      </Routes>
+
+  
+    </Router>
   );
 }
 
