6dadb7ebad7355be25c992181fcd0ecbb16340f4
Khyrelle Oberes <khynhoroberes.gmail.com>
2025-05-24 00:20:39 +0800

    I updated some placement of my elements

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './pages/Login/Login.js';
+import Home from './pages/Home/Home.js';
+import Signup from './pages/Signup/Signup.js';
+import Header from './components/Header/Header.js';
+import Footer from './components/Footer/Footer.js';
+import Search from './components/Header/Search/Search';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Header />
+      
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/Signup" element={<Signup />} />
+        <Route path="/Footer" element={<Footer />} />
+        <Route path="/Search" element={<Search />} />
+        <Route path="/Header" element={<Header />} />
+
+      </Routes>
+
+      <Footer />
+    </Router>
   );
 }
 
